b3c8b2d2882e7bb65f903133bf7cf015dd7e61f6
JanHulanicki <jan_hulanicki@poczta.onet.pl>
2019-10-09 19:53:49 +0200

    First Commit6.2

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,32 @@
+package com.kodilla.testing;
+
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+public class TestingMain {
+    public static void main(String[] args){
+        SimpleUser simpleUser=new SimpleUser("theForumUser");
+        String result =simpleUser.getUsername();
+        if(result.equals("theForumUser")){
+            System.out.println("test nr1 OK");
+        }else{
+            System.out.println("test nr1 error");
+        }
+        System.out.println("Test Calculator- pierwszy test jednostkowy:");
+        Calculator calcul =new Calculator();
+        int a=2;
+        int b=2;
+        int calculAdd=calcul.add(a,b);
+        if (calculAdd==(a+b)){
+            System.out.println("test calculAdd OK");
+        }else{
+            System.out.println("test calculAdd error");
+        }
+        int calculSub=calcul.subtract(a,b);
+        if (calculSub==(a-b)){
+            System.out.println("test calculSub OK");
+        }else{
+            System.out.println("test calculSub error");
+        }
+    }
+}
