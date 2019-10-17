935f8e2fcd572e7bf03368c8ea01daca02db9113
JanH <jan_hulanicki@poczta.onet.pl>
2019-10-17 09:28:40 +0200

    M7.1 Zadanie: Upiększacz tekstów

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,43 @@
+package com.kodilla.stream;
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+public class StreamMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to module 7 - Stream");
+        SaySomething saySomething = new SaySomething();
+        saySomething.say();
+        Processor processor = new Processor();
+        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
+        processor.execute(executeSaySomething);
+        Processor processor1 = new Processor();
+        Executor codeToExecute = () -> System.out.println("This is an example text1.");
+        processor1.execute(codeToExecute);
+        processor.execute(() -> System.out.println("This is an example text2."));
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+        //zadanie 1.
+        PoemBeautifier beautifier = new PoemBeautifier();
+        beautifier.beautify("Ala ma kota", textToDecorate -> textToDecorate.toUpperCase());
+        beautifier.beautify("Ala ma kota", textToDecorate -> new String(" ABC ") + textToDecorate + new String(" ABC"));
+        beautifier.beautify("Ala ma kota", textToDecorate -> textToDecorate.toLowerCase());
+        beautifier.beautify("Ala ma kota", textToDecorate -> textToDecorate.replace("Ala", "Tola") +
+                " " + textToDecorate.replace("kota", "psa"));
+        beautifier.beautify("Ala ma kota", textToDecorate -> textToDecorate.concat(" Ala nie ma psa"));
+    }
+}
+
+
