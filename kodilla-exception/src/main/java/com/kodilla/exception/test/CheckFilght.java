e68378f2ec2dc9f8390a837af2bea58a1c3d3a1c
JanH <jan_hulanicki@poczta.onet.pl>
2019-10-26 16:13:58 +0200

    M8.4 Zadanie: Wiedza o wyjątkach-poprawione

--- kodilla-exception/src/main/java/com/kodilla/exception/test/CheckFilght.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/CheckFilght.java
@@ -4,16 +4,16 @@ import java.util.Map;
 public class CheckFilght {
     Map<String, Boolean> mapFlight = new HashMap<>();
     public void findFlight(Flight flight) throws RouteNotFoundException {
-        if (!mapFlight.containsKey(flight.getArrivalAirport())) {
-            try {
+        try {
+            if (!mapFlight.containsKey(flight.getArrivalAirport())) {
                 throw new RouteNotFoundException("Destination airport does not found");
-            } catch (RouteNotFoundException e) {
-                System.out.println("Information: "+e.getMessage());
             }
-        } else {
-            System.out.println("Information: destination airport found ");
+            else System.out.println("Information: destination airport found ");
+
+            } catch(RouteNotFoundException e){
+                System.out.println("Information: " + e.getMessage());
+            }
         }
-    }
     public void addFlight(String airport, boolean permit) {
             mapFlight.put(airport,permit);
             //System.out.println(mapFlight);
